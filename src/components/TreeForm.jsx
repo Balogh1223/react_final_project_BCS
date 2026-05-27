4731cf0bdc9abd4a7abf420cb12c5f74a60f12cf
Balogh1223 <balogh1223@gmail.com>
2026-05-27 11:28:21 +0200

    TreeForm hibakeresés

--- src/components/TreeForm.jsx
+++ src/components/TreeForm.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 import Card from "../wrappers/Card";
+import Swal from "sweetalert2"
 
 const TreeForm = ({ sendDataToApp }) => {
 
@@ -7,20 +8,33 @@ const TreeForm = ({ sendDataToApp }) => {
     const darabszamRef = uesRef();
     const megjegyzesRef = useRef(); 
 
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        summarizeTreeData();
+    }
     
 
     const summarizeTreeData = () => {
         const fatipus = fatipusState
         const darabszam = darabszamRef.current.value
         const megjegyzes = megjegyzesRef.current.value
+
+        if(!fatipus || !darabszam || !megjegyzes){
+            Swal.fire({
+                icon: "error",
+                title: "Hiba",
+                text: "Kérem töltse ki a kötelező mezőket!"
+            })
+            return;
+        }
     }
 
-    
+
     return (
         <Card>
             <div>
                 <h2>Farendeles</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <label htmlFor="fatpus">Választható fatípus:</label>
                     <select name="" id="fatipus" onChange={(e) => setFatipus(e.target.value)}>
                         <option value="Barack csemete">Barackfa csemete</option>
@@ -35,6 +49,9 @@ const TreeForm = ({ sendDataToApp }) => {
                     <label htmlFor="megjegyzes">Megjegyzések:</label>
                     <input type="text" name="" id="megjegyzes" ref={megjegyzesRef}/>
 
+                    <button type="submit">
+                        Küldés
+                    </button>
                 </form>
             </div>
         </Card>
