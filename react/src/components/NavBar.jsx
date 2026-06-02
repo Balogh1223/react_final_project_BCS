77faa478f6b5250d2fd2ae7c5465dbb830ad223d
Balogh1223 <balogh1223@gmail.com>
2026-06-02 23:20:46 +0200

    NavBar létrehozása

--- react/src/components/NavBar.jsx
+++ react/src/components/NavBar.jsx
@@ -1,1 +1,18 @@
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../context/loginContext";
+
+const NabBar = () =>{
+    const {isLogged, logout} = useAuth()
+
+    return(
+        <nav>
+            <NavLink to='/' className={({isActive}) => isActive ? classes.active : classes.link}>Listanézet</NavLink>
+
+            {isLogged && <NavLink to='from' className={({isActive}) =>isActive ? classes.active : classes.link}>Új facsemete</NavLink>}
+
+            {!isLogged ? <NavLink to='login' className={({isActive}) =>isActive ? classes.active : classes.link}>Bejelentkezés</NavLink> : <a onClick={logout} className={classes.isActive}>Kijelentkezés</a>}
+        </nav>
+    )
+} 
+
+export default NavBar
