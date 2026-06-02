36e1fb8227d2a06effcabf5673aa5bf96eb9c662
Balogh1223 <balogh1223@gmail.com>
2026-06-02 22:57:44 +0200

    Login létrehozása

---
+++ react/src/components/Login.jsx
@@ -0,0 +1,59 @@
+import { useRef } from "react";
+import { useAuth } from "../context/loginContext";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+    const { login } = useAuth();
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+
+    e.preventDefault();
+    if (usernameRef.current.value !== "") {
+
+      try {
+
+        const response = await fetch('http://localhost:3000/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            username: usernameRef.current.value,
+            password: passwordRef.current.value
+          })
+        });
+
+
+        if (response.ok) {
+          const data = await response.json();
+          localStorage.setItem("token", data.token);
+          login();
+          navigate('/form');
+        } else { console.error('Sikertelen bejelentkezés') }
+
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+  };
+
+  return(
+    <>
+    <div>
+        <h3>Bejelentkezés</h3>
+        <form onSubmit={handleSubmit}></form>
+        <input type="text" placeholder="Felhasználónév" ref={usernameRef}/>
+        <input type="password" placeholder="Jelszó" ref={passwordRef}/>
+
+        <button type="submit">Belépés</button>
+    </div>
+    </>
+  )
+}
+
+
+
+export default Login;
